06827027a0d407cdaef523e47230d42452b5b624
bigblender2115 <shauryapaka@gmail.com>
2025-03-05 23:12:49 +0530

    debt tracker page added

--- test_1/lib/pages/home.dart
+++ test_1/lib/pages/home.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'timetable.dart'; 
 import 'bus.dart';       
-import 'mess.dart';  
+import 'mess.dart';
+import 'debt_tracker.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,133 +23,88 @@ class HomePage extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 63, 60, 153),
         centerTitle: true,
       ),
-      body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // First Button (Timetable)
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => TimetablePage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(20),
-                      backgroundColor: const Color.fromARGB(255, 255, 233, 125),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: SizedBox(
-                      width: 250,
-                      height: 250,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          'assets/icons/calender.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Timetable',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontSize: 30,
-                  ),
-                ),
-                SizedBox(height: 20), // Add space between the buttons
 
-                // Second Button (Bus Schedule)
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => BusPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(20),
-                      backgroundColor: const Color.fromARGB(255, 255, 233, 125),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: SizedBox(
-                      width: 250,
-                      height: 250,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          'assets/icons/Bus.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Bus Schedule',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontSize: 30,
-                  ),
-                ),
-                SizedBox(height: 20), // Add space between the buttons
+      //DRAWER HEADER
+      drawer: Drawer(
+        backgroundColor: Colors.purple[50],
+        child: Column(
+          children: [
+            DrawerHeader(child: Icon(
+              Icons.book,
+              size: 40
+            ))
+          ],
+        )
+      ),
 
-                // Third Button (Mess Menu)
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MessPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(20),
-                      backgroundColor: const Color.fromARGB(255, 255, 233, 125),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: SizedBox(
-                      width: 250,
-                      height: 250,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          'assets/icons/spoon.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Mess Menu',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    fontSize: 30,
-                  ),
-                ),
-              ],
-            ),
+
+
+      body: ListView(
+        children: [
+         ListTile(
+          minTileHeight: 165,
+          tileColor: Colors.deepPurple[800],
+            leading: Icon(Icons.directions_bus, size: 30, color: Colors.white),
+            minLeadingWidth: 30,
+            title: Text('Bus', textScaleFactor: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BusPage()),
+              );
+            },
           ),
-        ),
+          ListTile(
+            minTileHeight: 165,
+            tileColor: Colors.deepPurple[600],
+            leading: Icon(Icons.fastfood, size: 30, color: Colors.white),
+            title: Text('Mess', textScaleFactor: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MessPage()),
+              );
+            },
+          ),
+          ListTile(
+            minTileHeight: 165,
+            tileColor: Colors.deepPurple[400],
+            leading: Icon(Icons.schedule, size: 30, color: Colors.white),
+            title: Text('Timetable', textScaleFactor: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TimetablePage()),
+              );
+            },
+          ),
+          ListTile(
+            minTileHeight: 165,
+            tileColor: Colors.deepPurple[200],
+            leading: Icon(Icons.calendar_month, size: 30, color: Colors.white),
+            title: Text('Events and Holidays', textScaleFactor: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TimetablePage()),
+              );
+            },
+          ),
+          ListTile(
+            minTileHeight: 165,
+            tileColor: Colors.deepPurple[100],
+            leading: Icon(Icons.monetization_on, size: 30, color: Colors.white),
+            title: Text('Debt Tracker', textScaleFactor: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DebtTrackerPage()),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
